4b9ebdb8fee4bd69809b4668720738fbe0a351f2
Scott Barnard <scott@cullenjewellery.com>
2024-11-06 17:14:18 +1100

    feat(adyen): add Adyen pay UI

--- Resources/scripts/components/Adyen.vue
+++ Resources/scripts/components/Adyen.vue
@@ -3,34 +3,21 @@
     <div v-if="showErrorMessage">
       <PaymentErrorBlock :message="errorMessage" />
     </div>
-    <BaseButton
-      v-else
-      variant="primary"
-      type="submit"
-      class="mt-4 w-full justify-center"
-      :loading="isLoading"
-      @click="handleSubmit"
-    >
-      <template #left="slotProps">
-        <BaseIcon name="CreditCardIcon" :class="slotProps.class" />
-      </template>
 
-      {{
-        $t('payment_providers.pay_with_razorpay', {
-          amount: utils.formatMoney(orderData.order.amount, orderData.currency),
-        })
-      }}
-    </BaseButton>
+    <div class="payment" ref="paymentRef"></div>
   </div>
 </template>
 
 <script setup>
-import axios from 'axios'
-import { onMounted, reactive, watchEffect, inject, ref } from 'vue'
+import { onMounted, inject, ref } from 'vue'
 import { usePaymentProviderStore } from '~/scripts/stores/payment-provider'
-import PaymentSuccessBlock from './PaymentSuccessBlock.vue'
 import PaymentErrorBlock from './PaymentErrorBlock.vue'
 const { useRoute, useRouter } = window.VueRouter
+
+import { AdyenCheckout, Card } from '@adyen/adyen-web';
+import '@adyen/adyen-web/styles/adyen.css';
+
+const paymentRef = ref()
 
 const route = useRoute()
 const router = useRouter()
@@ -42,75 +29,56 @@ let errorMessage = ref(null)
 const paymentReceiptUrl = ref(null)
 const emit = defineEmits(['disable', 'reload'])
 
-let orderData = reactive({
-  order: {
-    id: '',
-    amount: '',
-  },
-  currency: null,
-  key: '',
-})
-
 const paymentProviderStore = usePaymentProviderStore()
 
-onMounted(() => {
-  let recaptchaScript = document.createElement('script')
-  recaptchaScript.setAttribute(
-    'src',
-    'https://checkout.razorpay.com/v1/checkout.js'
-  )
-  document.head.appendChild(recaptchaScript)
-
-  getData()
-})
-
-async function getData() {
+onMounted(async () => {
   try {
     let res = await paymentProviderStore.generatePayment(route.params.company)
-    
+
+
     if (res.data.error) {
       errorMessage.value = res.data.error.description
       showErrorMessage.value = true
       return
     }
 
-    Object.assign(orderData.order, res.data.order)
-    orderData.key = res.data.key
-    orderData.currency = res.data.currency
+    const checkout = await createAdyenCheckout(res.data);
+
+    new Card(checkout, {
+	    onError: () => {},
+	  }).mount(paymentRef.value)
+
     isLoading.value = false
   } catch (e) {
     console.error(e)
     isLoading.value = false
   }
-}
+})
 
-function handleSubmit() {
-  emit('disable', true)
-
-  var rzp1 = new Razorpay({
-    key: orderData.key,
-    currency: orderData.currency,
-    amount: orderData.order.amount,
-    order_id: orderData.order.id,
-    handler: function (response) {
-      paymentProviderStore
-        .confirmTransaction(response.razorpay_order_id, {
-          payment_id: response.razorpay_payment_id,
-          order_id: response.razorpay_order_id,
-          signature: response.razorpay_signature,
-          company_id: route.params.company,
-        })
-        .then((res) => {
-          paymentReceiptUrl.value = `/m/payments/pdf/${res.data.transaction.unique_hash}`
-          emit('reload', paymentReceiptUrl.value)
-        })
+async function createAdyenCheckout(data) {
+  const configuration = {
+    clientKey: 'test_OACDBSEAUFEEPFQTIEUU6YE3RYOSPKD6',
+    session: {
+      id: data.id,
+      sessionData: data.sessionData,
     },
-  })
-  rzp1.on('payment.failed', function (response) {
-    console.log(response.error.metadata.order_id)
-    console.log(response.error.metadata.payment_id)
-  })
+    environment: "test", // Change to 'live' for production
+    amount: data.amount,
+    locale: data.shopperLocale,
+    countryCode: data.countryCode,
 
-  rzp1.open()
+    onPaymentCompleted: (result, component) => {
+      console.log("result: " + result);
+    },
+    onPaymentFailed: (result, component) => {
+      console.error("result: " + result);
+    },
+    onError: (error, component) => {
+	    console.error(error.name, error.message, error.stack, component);
+	  }
+  };
+
+  return AdyenCheckout(configuration);
 }
+
 </script>
